3aff27ca4e37d3a4dced30c990b6d24f96f416c3
Yoshikazu Nishimura <yoshikazzzz@gmail.com>
2018-09-18 03:27:59 +0900

    update ERC721 contracts

--- contracts/ERC721SampleSign.sol
+++ contracts/ERC721SampleSign.sol
@@ -12,7 +12,7 @@ contract ERC721Sample {
     mapping(address => uint256) private ownerTokenLength;
     mapping(uint256 => bool) private tokenExists;
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _tokenId, string _signature);
+    event BatchTransfer(string _pointer);
 
     constructor() public {
         owner = msg.sender;
@@ -27,44 +27,29 @@ contract ERC721Sample {
         return tokenOwners[_tokenId];
     }
     
-    function removeFromTokenList(address _owner, uint256 _tokenId) internal {
-        for(uint256 i = 0; ownerTokens[owner][i] != _tokenId; i++){
-            ownerTokens[_owner][i] = 0;
-        }
-    }
-    
-    function _transfer(address _from, address _to, uint256 _tokenId, string _signature) internal {
+    function _transfer(address _from, address _to, uint256 _tokenId) internal {
         if(_from != address(0)){
             balances[_from] -= 1;
-            removeFromTokenList(_from, _tokenId);
         }
         tokenExists[_tokenId] = true;
         tokenOwners[_tokenId] = _to;
         ownerTokens[_to][ownerTokenLength[_to]] = _tokenId;
         ownerTokenLength[_to] += 1;
         balances[_to] += 1;
-        emit Transfer(_from, _to, _tokenId, _signature);
-    }
-    
-    function transfer(address _to, uint256 _tokenId, string _signature) public {
-        require(tokenExists[_tokenId]);
-        require(msg.sender == ownerOf(_tokenId));
-        require(msg.sender != _to);
-        require(_to != address(0));
-        
-        _transfer(msg.sender, _to, _tokenId, _signature);
     }
     
     function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint tokenId){
         return ownerTokens[_owner][_index];
     }
-    
-    function mint(address _to, uint256 _tokenId, string _signature) public {
+
+    function multiTransfer(address[] _from, address[] _to, uint256[] _tokenId, string _pointer) public {
         require(msg.sender == owner);
-        require(!tokenExists[_tokenId]);
-        require(_to != address(0));
-        totalSupply += 1;
-        
-        _transfer(address(0), _to, _tokenId, _signature);
+        require(_from.length == _to.length);
+        require(_from.length == _tokenId.length);
+        for(uint256 i = 0; i<_from.length; i++) {
+            _transfer(_from[i], _to[i], _tokenId[i]);
+        }
+        emit BatchTransfer(_pointer);
     }
+    
 }
